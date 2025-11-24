f7f52e8f84ab7973d85dd41790c131fd71fc00c9
Bibillash-dot <sabillashley@gmail.com>
2025-11-24 11:38:36 +0800

    Create lib messages.js

---
+++ lib messages.js
@@ -0,0 +1,14 @@
+module.exports = {
+    menu: (botName) => {
+        return `
+👋 Halo! Ini adalah *${botName}*
+
+📌 *Menu Utama*
+• .menu
+• .owner
+• .ai (coming soon)
+
+🔧 Bot masih tahap pengembangan!
+        `;
+    }
+};
